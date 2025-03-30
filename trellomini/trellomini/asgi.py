98448f2839e8d0c4930a47f4c23131d7e159c025
Patrick Kelly <pkelly@babelstreet.com>
2025-03-30 17:26:06 -0400

    Changed the name of the templates' project file and it's correlating references. Created an environment file with the development's secret key and requirements.

--- trellomini/trellomini/asgi.py
+++ trellomini/trellomini/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoWebTemplate project.
+ASGI config for trellomini project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWebTemplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trellomini.settings')
 
 application = get_asgi_application()
