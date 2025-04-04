27714452e9b7d5a2d0c82418e17ed0e33f546f36
Patrick Kelly <patrick.kellyse@gmail.com>
2025-04-04 14:30:50 -0400

    Update settings.py

--- trellomini/trellomini/settings.py
+++ trellomini/trellomini/settings.py
@@ -24,13 +24,12 @@ load_dotenv(os.path.join(BASE_DIR, '.env')) # Load environment variables from .e
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.SECRET_KEY
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.DEBUG
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = env.ALLOWED_HOSTS
-
 
 # Application definition
 
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
     # ...
     # Add created apps here
     'users',
+    'projects',
 ]
 
 MIDDLEWARE = [
