da890507b5a9c25be6d351bb07443d0af1607961
Patrick Kelly <pkelly@babelstreet.com>
2025-04-04 15:07:09 -0400

    Created the models for Project and TAsks, enabled status choices.

--- trellomini/projects/models.py
+++ trellomini/projects/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ("TODO", "To Do"),
+        ("DOING", "Doing"),
+        ("DONE", "Done"),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="TODO")
+    project = models.ForeignKey(Project, related_name="tasks", on_delete=models.CASCADE)
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.project.name} - {self.status}"
