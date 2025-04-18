22e204ed5e5438747ec92bba6863033c52674a8b
Clark Brown III <33003810+chbrowniii@users.noreply.github.com>
2025-04-17 22:31:48 -0400

    Created templates and views

--- trellomini/trellomini/views.py
+++ trellomini/trellomini/views.py
@@ -1,5 +1,38 @@
 # trellomini/views.py
 from django.shortcuts import render
 
+
 def home_view(request):
-    return render(request, 'home.html') # Render the home page
+    return render(request, "home.html")  # Render the home page
+
+
+def dashboard_view(request):
+    return render(request, "/dashboard/dashboard.html")  # Render the dashboard page
+
+
+def project_view(request):
+    return render(request, "/projects/project.html")  # Render the project page
+
+
+def create_project_view(request):
+    return render(
+        request, "/projects/create_project.html"
+    )  # Render the create project page
+
+
+def edit_project_view(request):
+    return render(
+        request, "/projects/edit_project.html"
+    )  # Render the edit project page
+
+
+def task_view(request):
+    return render(request, "/tasks/task.html")  # Render the task page
+
+
+def create_task_view(request):
+    return render(request, "/tasks/create_task.html")  # Render the create task page
+
+
+def edit_task_view(request):
+    return render(request, "/tasks/edit_task.html")  # Render the create task page
